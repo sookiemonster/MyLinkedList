cee45648d57f2321a14dfacb77f756be05bd9680
sookiemonster <dsooknanan20@stuy.edu>
2021-01-06 14:13:53 -0500

    Added MyLinkedList.java, added constructor, added size() method, added add().

---
+++ MyLinkedList.java
@@ -0,0 +1,27 @@
+public class MyLinkedList {
+  private int size;
+  private Node start, end;
+
+  public MyLinkedList() {
+    this.size = 0;
+    this.start = null;
+    this.end = null;
+  }
+
+  public int size() {
+    return this.size;
+  }
+
+  public boolean add(String value) {
+    if (this.size() == 0) {
+      Node temp = new Node(value, null, null);
+      this.start = temp;
+      this.end = temp;
+    } else {
+      Node temp = new Node(value, null, this.end);
+      this.end = temp;
+    }
+    size++;
+    return true;
+  }
+}
